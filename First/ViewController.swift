9e232c97d4db6770a5c9cae6ff6b800c433d4c90
Khalid <i.flaming.spark@gmail.com>
2017-01-30 02:32:18 +0300

    hello world

---
+++ First/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Khalid on 1/30/17.
+//  Copyright © 2017 Khalid Almaimouni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
